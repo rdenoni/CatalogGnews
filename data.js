5bad7a73e654f51c958a25a9237916bc59149a2d
Rafael Denoni <44037286+rdenoni@users.noreply.github.com>
2025-05-14 08:01:50 -0300

    Delete data.js

--- data.js
+++
@@ -1,4 +0,0 @@
-const cardData = [
-    { "id": "1", "name": "Cartão Teste 1", "tag": "MKT", "code": "MKT1234", "created": "2025-05-09T00:00:00Z", "lastEdited": "2025-05-09T00:00:00Z" },
-    { "id": "2", "name": "Cartão Teste 2", "tag": "DEV", "code": "DEV5678", "created": "2025-05-09T00:00:00Z", "lastEdited": "2025-05-09T00:00:00Z" }
-];
